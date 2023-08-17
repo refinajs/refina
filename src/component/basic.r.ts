f31361862cf6eb06877f18da445aa6345e13c7d0
_Kerman <KermanX@qq.com>
2023-08-17 21:52:10 +0800

    add basic components

--- src/component/basic.r.ts
+++ src/component/basic.r.ts
@@ -11,21 +11,95 @@ import {
   triggerComponent,
 } from "./index";
 import { HTMLElementComponent } from "../dom";
+import { ViewRender } from "../view";
+import {
+  OutputComponent,
+  OutputComponentContext,
+  outputComponent,
+} from "./output";
+
+@statusComponent
+export class Div extends StatusComponent {
+  main(
+    _: StatusComponentContext<this>,
+    inner: D<string | number | ViewRender>
+  ) {
+    _._div({}, inner);
+  }
+}
+declare module "./index" {
+  interface StatusComponents {
+    div: Div;
+  }
+}
+
+@outputComponent
+export class Span extends OutputComponent {
+  main(
+    _: OutputComponentContext<this>,
+    inner: D<string | number | ViewRender>
+  ) {
+    _._span({}, inner);
+  }
+}
+declare module "./index" {
+  interface OutputComponents {
+    span: Span;
+  }
+}
+
+@outputComponent
+export class Br extends OutputComponent {
+  main(_: OutputComponentContext<this>) {
+    _._br();
+  }
+}
+declare module "./index" {
+  interface OutputComponents {
+    br: Br;
+  }
+}
+
+@outputComponent
+export class H1 extends OutputComponent {
+  main(
+    _: OutputComponentContext<this>,
+    inner: D<string | number | ViewRender>
+  ) {
+    _._h1({}, inner);
+  }
+}
+declare module "./index" {
+  interface OutputComponents {
+    h1: H1;
+  }
+}
+
+@outputComponent
+export class P extends OutputComponent {
+  main(
+    _: OutputComponentContext<this>,
+    inner: D<string | number | ViewRender>
+  ) {
+    _._p({}, inner);
+  }
+}
+
+declare module "./index" {
+  interface OutputComponents {
+    p: P;
+  }
+}
 
 @triggerComponent
 export class Button extends TriggerComponent {
   main(_: TriggerComponentContext<MouseEvent, this>, text: D<string>) {
-    // _._button(
-    //   {
-    //     onclick: _.$fire,
-    //   },
-    //   getD(text)
-    // );
-    if (_._cbButton({}, getD(text))) {
-      if (_.$.onClick()) {
-        _.$fire(_.$.$ev);
-      }
-    }
+    _._button(
+      {
+        onclick: _.$fire,
+      },
+      getD(text)
+    );
   }
 }
 declare module "./index" {
@@ -43,6 +117,7 @@ export class TextInput extends StatusComponent {
       _._t(label);
       _.$ref(this.inputEl) &&
         _._input({
+          type: "text",
           value: getD(value),
           oninput: () => {
             _.$setD(value, this.inputEl.current!.node.value);
@@ -59,53 +134,66 @@ declare module "./index" {
   }
 }
 
-// type Key<E, K extends keyof E> =
-//   | ((this: any, ev: any) => any)
-//   | null extends E[K]
-//   ? K
-//   : never;
-// type Keys<E> = {
-//   [K in keyof E]: Key<E, K>;
-// }[keyof E];
-// type EvName<K extends string> = K extends `on${infer N}` ? N : never;
-// type HTMLELementEvs<E> = {
-//   [K in Keys<E> & string as EvName<K>]: E[K] extends ((this: any, ev: infer Ev) => any) | null
-//     ? Ev
-//     : never;
-// };
-// type CbButtonEvs = HTMLELementEvs<HTMLButtonElement>;
-type CbButtonEvs = HTMLElementEventMap;
-@callbackComponent
-export class CbButton extends CallbackComponent<CbButtonEvs> {
-  main(_: CallbackComponentContext<CbButtonEvs, this>, text: D<string>) {
-    _._button(
-      Object.fromEntries(
-        [...this.$listendEvs].map((ev) => [`on${ev}`, _.$firer(ev)] as const)
-      ),
-      getD(text)
-    );
-  }
-}
-declare module "./index" {
-  interface CallbackComponents {
-    cbButton: CbButton;
-  }
-}
-
 @statusComponent
 export class ToggleButton extends StatusComponent {
   main(_: StatusComponentContext<this>, text: D<string>) {
-    // console.log(_.$state);
-    // _.button( "111");
-    if (_.cbButton(text)) {
-      if (_.$.onClick()) {
-        _.$toggle();
-      }
+    if (_.button(text)) {
+      _.$toggle();
     }
   }
 }
 declare module "./index" {
   interface StatusComponents {
     toggleButton: ToggleButton;
+  }
+}
+
+@statusComponent
+export class Checkbox extends StatusComponent {
+  inputEl = ref<HTMLElementComponent<"input">>();
+  main(
+    _: StatusComponentContext<this>,
+    label: D<string>,
+    value: D<boolean> = this.inputEl.current?.node.checked ?? false
+  ) {
+    _._label({}, () => {
+      _._t(label);
+      _.$ref(this.inputEl) &&
+        _._input({
+          type: "checkbox",
+          onchange: () => {
+            _.$setD(value, this.inputEl.current!.node.checked);
+            _.$status = this.inputEl.current!.node.checked;
+          },
+        });
+    });
+  }
+}
+declare module "./index" {
+  interface StatusComponents {
+    checkbox: Checkbox;
+  }
+}
+
+@outputComponent
+export class Ul extends OutputComponent {
+  main<T>(
+    _: OutputComponentContext<this, any>,
+    data: D<Iterable<T>>,
+    key: keyof T | ((item: T, index: number) => D<string>),
+    body: (item: T, index: number) => void
+  ): void {
+    _._ul({}, () => {
+      _.for(data, key, (item, index) => {
+        _._li({}, () => {
+          body(item, index);
+        });
+      });
+    });
+  }
+}
+declare module "./index" {
+  interface OutputComponents {
+    ul: Ul;
   }
 }
--- src/test.r.ts
+++ src/test.r.ts
@@ -1,9 +1,60 @@
-import { Button, d, ref, view } from "./lib";
+import { HTMLElementComponent } from "./dom";
+import { Button, bySelf, d, ref, view } from "./lib";
 const on = d(false);
 const t = d.trim("hello world");
 let n = 4;
-const btn = ref<Button>();
+const p = ref<HTMLElementComponent<"p">>();
+let s = [1, 2, 3];
+function ff(): never {
+  throw new Error("test");
+}
 view((_) => {
+  _.ul(s, bySelf, (i) => {
+    if (_._cbButton({}, i as string)) {
+      if (_.$.onClick()) {
+        console.log("+", _.$state, i);
+        n += i as number;
+      }
+      if (_.$.onContextmenu()) {
+        n -= i as number;
+        console.log(_.$.$ev);
+        _.$.$preventDefault();
+      }
+    }
+  });
+  if (_.h1(`${on}` + n)) {
+    console.log(_.$);
+  }
+  if ((() => false as const)()) {
+    addEventListener;
+  }
+  if (_.checkbox("ON/OFF")) {
+    _.p("ON");
+  }
+  _.br();
+  if (_.checkbox("ON/OFF")) {
+    _.p("ON");
+  }
+  // if (_.toggleButton("1")) {
+  //   _.$clear(p);
+  //   if (_.$ref(p)) {
+  //     _.$;
+  //     _._p({}, "MENU" + n);
+  //   }
+  //   if (_.button("!!!")) {
+  //     _.$;
+  //     _.$ref(p);
+  //   }
+  //   if (_.cbButton("!11")) {
+  //     _.$ev;
+  //     _.$;
+  //     if (_.$.onClick()) {
+  //       _.$toggle();
+  //     }
+  //     _.$ref(ref<{ a: 111 }>());
+  //   }
+  //   //&& _.$ &&
+  // }
   // _.$cls`${on.value ? "bg-blue-500" : ""}`;
   // _._pre({}, `$${t}$`);
   // if (_.$ref(btn) && _.button("Toggle")) on.value = !on.value;
@@ -11,16 +62,16 @@ view((_) => {
   // _._t(n);
   // if (_.button("-")) n--;
 
-  // if (_.cbButton("LR click")) {
+  // if (_._cbButton({}, "LR click")) {
   //   if (_.$.onClick()) {
-  //     console.log(_.$.$ev);
   //     n++;
   //   }
   //   if (_.$.onContextmenu()) {
-  //     console.log(_.$.$ev);
+  //     _.$.$ev.preventDefault();
   //     n = 0;
   //   }
   // }
+  // _._p({}, n);
   // if (_._cbButton({}, "LR click2")) {
   //   if (_.$.onClick()) {
   //     console.log(_.$.$ev);
@@ -42,10 +93,12 @@ view((_) => {
   //     }
   //   }
   // }
-  if (_.button("ABC")) {
-    n++;
-  }
-  _._p({}, "MENU" + n);
+
+  // if (_.button("ABC")) {
+  //   n++;
+  //   _.$ev;
+  // }
+  // _.$ev;
   //_._t("1111111")
   // _.textInput("Type here: ", t);
   // _.textInput("Type here2: ", t);
