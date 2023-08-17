423c54eb9d7c56638f17d0c2621e3a5f4adb4b20
_Kerman <KermanX@qq.com>
2023-08-17 17:32:53 +0800

    add outputComponent

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,12 +1,15 @@
 import { CallbackComponentFuncs } from "./callback";
+import { OutputComponentFuncs } from "./output";
 import { StatusComponentFuncs } from "./status";
 import { TriggerComponentFuncs } from "./trigger";
 
 export * from "./callback";
 export * from "./component";
+export * from "./output";
 export * from "./status";
 export * from "./trigger";
 
 export type ComponentFuncs<C> = TriggerComponentFuncs<C> &
   StatusComponentFuncs<C> &
-  CallbackComponentFuncs<C>;
+  CallbackComponentFuncs<C> &
+  OutputComponentFuncs<C>;
---
+++ src/component/output.ts
@@ -0,0 +1,50 @@
+import { ToFullContext, contextFuncs, Context } from "../context";
+import {
+  Component,
+  ComponentConstructor,
+  IntrinsicComponentContext,
+  ComponentFuncArgs,
+} from "./component";
+
+export abstract class OutputComponent extends Component {
+  abstract main(_: OutputComponentContext<this>, ...args: any[]): void;
+}
+export class IntrinsicOutputComponentContext<
+  S extends OutputComponent,
+  C = any,
+> extends IntrinsicComponentContext<S, C> {}
+export type OutputComponentContext<
+  S extends OutputComponent,
+  C = any,
+> = ToFullContext<C, IntrinsicOutputComponentContext<S, C>>;
+export function outputComponent<S extends OutputComponent>(
+  ctor: ComponentConstructor<S>
+) {
+  if (!ctor.name) throw new Error(`Component class must have name.`);
+  const name = ctor.name[0].toLowerCase() + ctor.name.slice(1);
+  contextFuncs[name] = function (this: Context, ckey, ...args) {
+    const component = this.beginComponent(ckey, ctor);
+
+    const context = new IntrinsicOutputComponentContext(this, component);
+
+    component.main(context as any as OutputComponentContext<S>, ...args);
+
+    if (!context.$classesArgUsed) {
+      context.$firstHTMLELement?.addClasses(context.$classesArg);
+    }
+
+    context.$callHookAfterThisComponent();
+
+    this.endComponent(ckey);
+
+    return false;
+  };
+  return ctor;
+}
+export interface OutputComponents extends Record<string, OutputComponent> {}
+
+export type OutputComponentFuncs<C> = {
+  [K in keyof OutputComponents]: OutputComponents[K] extends C
+    ? (...args: ComponentFuncArgs<OutputComponents[K]>) => never
+    : never;
+};
