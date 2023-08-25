ae8c3733bf4d78ab35f3ee74d75b788790eae5d2
_Kerman <KermanX@qq.com>
2023-08-25 15:57:19 +0800

    delete useless code

--- src/components/rButton.r.ts
+++ src/components/rButton.r.ts
@@ -1,6 +1,5 @@
 import { Content } from "../dom";
 import { D, TriggerComponent, TriggerComponentContext, triggerComponent } from "../lib";
-// import { rAppendIconSymbol, rPrependIconSymbol } from "./rIcon.r";
 
 @triggerComponent("rButton")
 export class RButton extends TriggerComponent<MouseEvent> {
@@ -9,22 +8,6 @@ export class RButton extends TriggerComponent<MouseEvent> {
     if (_.button(inner, disabled)) {
       _.$fire(_.$ev);
     }
-    // _._button(
-    //   {
-    //     onclick: _.$fire,
-    //     disabled: getD(disabled),
-    //   },
-    //   () => {
-    //     _.$runIfProvided(rPrependIconSymbol, ({ name, stacked }) => {
-    //       _.$cls`${stacked ? "block" : "inline"}`;
-    //       _.rIcon(name);
-    //     });
-    //     _.span(inner);
-    //     _.$runIfProvided(rAppendIconSymbol, ({ name, stacked }) => {
-    //       _.rIcon(name);
-    //     });
-    //   },
-    // );
   }
 }
 
