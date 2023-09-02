b90f9dfd97c2eca6cb83257e42beefc8b5d0507b
_Kerman <KermanX@qq.com>
2023-09-02 15:58:48 +0800

    add rRouterLink

---
+++ src/components/router/link.r.ts
@@ -0,0 +1,16 @@
+import { Content, D, OutputComponent, OutputComponentContext, getD, outputComponent } from "../../lib";
+
+@outputComponent("rRouterLink")
+export class RRouterLink extends OutputComponent {
+  main(_: OutputComponentContext<this>, target: D<string>, content: D<Content>): void {
+    let href = getD(target);
+    // TODO: normalize href
+    _.a(target, content);
+  }
+}
+
+declare module "../../component/index" {
+  interface OutputComponents {
+    rRouterLink: RRouterLink;
+  }
+}
