60ea0993291f5197f651724506d9a0c2ec3840bc
_Kerman <KermanX@qq.com>
2023-09-08 22:21:42 +0800

    add `rPassword` component

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,6 +2,7 @@ import { RFormData } from "./base";
 
 export * from "./rCheckbox.r";
 export * from "./rForm.r";
+export * from "./rPassword.r";
 export * from "./rTextField.r";
 
 export function formData<T extends RFormData>(
---
+++ src/components/form/rPassword.r.ts
@@ -0,0 +1,49 @@
+import { HTMLElementComponent, ref } from "../../lib";
+import { FormComponent, FormComponentContext, formComponent } from "./base";
+
+@formComponent("rPassword")
+export class RPassword<T extends object> extends FormComponent<string, T> {
+  defaultValue = "";
+  inputEl = ref<HTMLElementComponent<"input">>();
+  focused = false;
+  main(_: FormComponentContext<string, T, this>): void {
+    _.$cls`relative`;
+    _.div(() => {
+      _.$cls`border-black border-2 rounded pt-7 pb-1 px-4 text-base`;
+      _.$ref(this.inputEl) &&
+        _._input({
+          type: "password",
+          value: _.$data,
+          onfocus: () => {
+            this.focused = true;
+            _.$refresh();
+          },
+          onblur: () => {
+            this.focused = false;
+            _.$refresh();
+          },
+          oninput: () => {
+            this.activited = true;
+            _.$data = this.inputEl.current!.node.value;
+            _.$refresh();
+          },
+        });
+      _.$cls`absolute ${
+        this.focused || this.inputEl.current!.node.value.length > 0
+          ? "text-sm top-2 start-2"
+          : "text-base top-4 start-4 "
+      } ${this.valid !== true ? "text-red-500" : ""}`;
+      _.div(this.label);
+      if (this.valid !== true) {
+        _.$cls`relative text-red-500 text-sm`;
+        _.div(this.valid);
+      }
+    });
+  }
+}
+
+declare module "./base" {
+  interface FormComponents {
+    rPassword: RPassword<any>;
+  }
+}
