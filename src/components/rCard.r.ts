636709d073e0228548fe414bf930c1b938d6a7bc
_Kerman <KermanX@qq.com>
2023-09-22 14:21:32 +0800

    update component styles

--- src/components/rCard.r.ts
+++ src/components/rCard.r.ts
@@ -13,16 +13,16 @@ export class RCard extends TriggerComponent<number> {
     const disabledValue = getD(disabled);
     _.$cls`bg-[#e3e3e3] relative rounded m-3`;
     _.div(() => {
-      _.$cls`pt-4 px-6 pb-[10px] text-xl`;
+      _.$cls`pt-4 px-5 pb-[10px] text-xl bg-gray-400 rounded-t`;
       _.div(heading);
       _.$cls`p-4 text-sm`;
       _.div(body);
       if (buttonsValue.length > 0) {
-        _.$cls`p-2 flex items-center min-h-[52px]`;
+        _.$cls`flex items-center h-10`;
         _.div(() => {
           _.for(buttons, byIndex, (btn, index) => {
-            _.$cls`px-2 h-9 me-2`;
-            _.rButton(btn, disabledValue[index]) && _.$fire(index);
+            _.$cls`px-2 h-full flex-initial w-64 rounded bg-gray-300 hover:bg-gray-400 transition-colors`;
+            _.button(btn, disabledValue[index]) && _.$fire(index);
           });
         });
       }
--- src/components/table/styles.ts
+++ src/components/table/styles.ts
@@ -3,7 +3,7 @@ import { $cls } from "../../lib";
 export default {
   table: $cls`m-3 border border-black rounded shadow`,
   thead: $cls`bg-gray-400`,
-  th: $cls`p-2`,
+  th: $cls`p-2 hover:bg-gray-500 transition`,
   tbody: $cls`bg-gray-200`,
   tr: $cls`border-b border-gray-400 hover:bg-gray-300 transition`,
   td: (hover: boolean) =>
