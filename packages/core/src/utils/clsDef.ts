b7cdfa71783f00103a1d101a7b10ed5daf0c16f3
_Kerman <KermanX@qq.com>
2023-09-27 11:50:35 +0800

    update class funcs

--- packages/core/src/utils/clsDef.ts
+++ packages/core/src/utils/clsDef.ts
@@ -1,11 +1,20 @@
 import { Context } from "../context";
 
-export function $cls(cls: string): (_: Context) => true;
-export function $cls(
+export function $clsStr<const S extends string>(cls: S): S;
+export function $clsStr(template: TemplateStringsArray, ...args: any[]): string;
+export function $clsStr(s: string | TemplateStringsArray, ...args: any[]) {
+  if (typeof s === "string") {
+    return s;
+  }
+  return String.raw(s, ...args);
+}
+
+export function $clsFunc(cls: string): (_: Context) => true;
+export function $clsFunc(
   template: TemplateStringsArray,
   ...args: any[]
 ): (_: Context) => true;
-export function $cls(...args: any[]) {
+export function $clsFunc(...args: any[]) {
   //@ts-ignore
   return (_: Context) => _.$cls(...args);
 }
