ba968f2428dcfb4cccc901436fe6930dbac0154c
_Kerman <KermanX@qq.com>
2023-10-01 13:33:32 +0800

    add ref merging

--- packages/core/src/data/ref.ts
+++ packages/core/src/data/ref.ts
@@ -2,6 +2,25 @@ export interface Ref<T> {
   current: T | null;
 }
 
-export function ref<T extends object>(current: T | null = null): Ref<T> {
+export function ref<T>(current: T | null = null): Ref<T> {
   return { current };
+}
+
+export interface MergedRef<T> extends Ref<T> {
+  mergedCurrent: T | null;
+}
+
+export function mergeRefs<T>(...refs: Ref<T>[]): MergedRef<T> {
+  return {
+    mergedCurrent: null,
+    get current() {
+      return this.mergedCurrent;
+    },
+    set current(v) {
+      this.mergedCurrent = v;
+      refs.forEach((ref) => {
+        ref.current = v;
+      });
+    },
+  };
 }
