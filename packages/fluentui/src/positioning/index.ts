17569cfe4d6022226e5560c881ff9ec4d8591108
_Kerman <KermanX@qq.com>
2023-10-08 14:42:06 +0800

    add `mergeArrowOffset` and update exports

--- packages/fluentui/src/positioning/index.ts
+++ packages/fluentui/src/positioning/index.ts
@@ -8,6 +8,7 @@ export * from "./constants";
 export * from "./createArrowStyles";
 export * from "./createSlideStyles";
 export * from "./types";
+export * from "./utils";
 
 addCustomContextFunc(
   "usePositioning",
---
+++ packages/fluentui/src/positioning/utils/index.ts
@@ -0,0 +1,5 @@
+export * from "./fromFloatingUIPlacement";
+export * from "./mergeArrowOffset";
+export * from "./parseFloatingUIPlacement";
+export * from "./resolvePositioningShorthand";
+export * from "./toFloatingUIPlacement";
