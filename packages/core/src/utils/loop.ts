85346062d89f7a709fdf7ac83d7f088aa14e3f19
_Kerman <KermanX@qq.com>
2023-10-11 23:12:29 +0800

    refact loop code

--- packages/core/src/utils/loop.ts
+++ packages/core/src/utils/loop.ts
@@ -1,36 +1,29 @@
 import { Context, addCustomContextFunc } from "../context";
 import { D, getD } from "../data/index";
 
-declare module "../context" {
-  interface CustomContext<C> {
-    for: never extends C
-      ? <T = unknown>(
-          arr: D<Iterable<T>>,
-          key: keyof T | ((item: T, index: number) => D<string>),
-          body: (item: T, index: number) => void,
-        ) => void
-      : never;
-    forRange: never extends C
-      ? (times: D<number>, body: (index: number) => void) => void
-      : never;
-  }
+type KeyHelper<T> = {
+  [K in keyof T]: T[K] extends string | number | bigint ? K : never;
+}[keyof T];
+
+export type KeyFunc<T> =
+  | KeyHelper<T>
+  | ((item: T, index: number) => D<string | number>);
+
+function normalizeKey<T>(key: KeyFunc<T>) {
+  return typeof key === "function" ? key : byProp(key);
 }
+
 addCustomContextFunc("for", function <
   T,
->(this: Context, ckey: string, arr: D<Iterable<T>>, key: keyof T | ((item: T, index: number) => D<string>), body: (item: T, index: number) => void) {
+>(this: Context, ckey: string, arr: D<Iterable<T>>, key: KeyFunc<T>, body: (item: T, index: number) => void) {
   this.$app.pushKey(ckey);
-  let k: any;
-  if (typeof key === "string") {
-    k = (item: T) => item[key];
-  } else {
-    k = key;
-  }
+  const keyFunc = normalizeKey(key);
   let i = 0;
   for (const item of getD(arr)) {
-    const key = k(item, i);
-    this.$app.pushKey(key);
+    const key = keyFunc(item, i);
+    this.$app.pushKey(key.toString());
     body(item, i);
-    this.$app.popKey(key);
+    this.$app.popKey(key.toString());
     i++;
   }
   this.$app.popKey(ckey);
@@ -53,13 +46,24 @@ addCustomContextFunc(
   },
 );
 
-export const byIndex = (_: any, index: number) => index.toString();
+declare module "../context" {
+  interface CustomContext<C> {
+    for: never extends C
+      ? <T = unknown>(
+          arr: D<Iterable<T>>,
+          key: keyof T | ((item: T, index: number) => D<string>),
+          body: (item: T, index: number) => void,
+        ) => void
+      : never;
+    forRange: never extends C
+      ? (times: D<number>, body: (index: number) => void) => void
+      : never;
+  }
+}
+
+export const byIndex = (_: any, index: number) => index;
 export const bySelf = (item: any) => `${item}`;
 export const byProp =
-  <T>(
-    key: {
-      [K in keyof T]: T[K] extends string | number | bigint ? K : never;
-    }[keyof T],
-  ) =>
+  <T>(key: KeyHelper<T>) =>
   (obj: T) =>
     `${obj[key]}`;
