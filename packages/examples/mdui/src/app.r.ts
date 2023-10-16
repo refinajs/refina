abd497620280d33e853a8d795975cc99acfe2166
Zecyel <zecyel@163.com>
2023-10-16 16:40:21 +0800

    Impl theme in refina/mdui

--- packages/examples/mdui/src/app.r.ts
+++ packages/examples/mdui/src/app.r.ts
@@ -4,8 +4,7 @@ import "@refina/mdui";
 import "@refina/mdui/styles.css";
 
 app((_) => {
-  _.$rootCls`mdui-theme-primary-red`;
-  _.$rootCls`mdui-theme-accent-indigo`;
+  _.provideMDTheme("red", "orange");
   // _.t`Hello, world!`;
   // _._br();
   // _.mdButton("Click me!");
