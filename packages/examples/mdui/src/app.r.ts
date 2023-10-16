33c5471c5517f5caec74de17cd92c23975157e4d
Zecyel <zecyel@163.com>
2023-10-16 14:13:36 +0800

    remove unnecessary items in example/mdui/index.html

--- packages/examples/mdui/src/app.r.ts
+++ packages/examples/mdui/src/app.r.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 import { app } from "refina";
 import "@refina/mdui";
-// import "@refina/mdui/styles.css";
+import "@refina/mdui/styles.css";
 
 app((_) => {
   _.$rootCls`mdui-theme-primary-red`;
