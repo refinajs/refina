1db11dbd86ec0b3d60c55393da23c066d4e00fe8
_Kerman <KermanX@qq.com>
2023-10-17 20:31:52 +0800

    fix embed

--- packages/core/src/utils/embed.r.ts
+++ packages/core/src/utils/embed.r.ts
@@ -1,54 +1,39 @@
+import { OutputComponent } from "../component";
 import { Prelude } from "../constants";
-import { OutputComponent } from "../component/output";
 import { Context, IntrinsicContext } from "../context";
 import { D, getD } from "../data";
 import { Content } from "../dom";
+import { View } from "../view";
 
 const contentCache = new Map<string, Content<any>>();
 
+type EmbededContent<Args extends any[]> =
+  | Content<Args>
+  | (() => Promise<{
+      default: Content<Args>;
+    }>);
+
 @Prelude.outputComponent("embed")
 export class Embed extends OutputComponent {
-  main<Args extends any[]>(
-    _: Context,
-    content: D<
-      | Content<Args>
-      | (() => Promise<{
-          default: Content<Args>;
-        }>)
-    >,
-    ...args: Args
-  ): void {
-    const contentValue = getD(content);
+  main<Args extends any[]>(_: Context, content: D<EmbededContent<Args>>, ...args: Args): void {
+    const contentValue: EmbededContent<Args> = contentCache.get(this.$ikey) ?? getD(content);
 
     const context = new IntrinsicContext(_.$app);
 
-    function processContent(content: Content<Args>) {
-      if (typeof content === "function") {
-        content(context as unknown as Context, ...args);
+    if (typeof contentValue === "function") {
+      const ret: unknown = contentValue(context as unknown as Context, ...args);
+      if (ret instanceof Promise) {
+        _.t`Loading module...`;
+        ret.then((r) => {
+          contentCache.set(this.$ikey, r.default);
+          _.$app.update();
+        });
       } else {
-        _.t(String(content));
+        _.$firstDOMNode = context.$firstDOMNode;
+        _.$firstHTMLELement = context.$firstHTMLELement;
       }
-    }
-
-    if (contentCache.has(this.$ikey)) {
-      processContent(contentCache.get(this.$ikey)!);
     } else {
-      if (typeof contentValue === "function") {
-        const ret = contentValue(context as unknown as Context, ...args);
-        if (ret instanceof Promise) {
-          _.t`Loading module...`;
-          ret.then((r) => {
-            contentCache.set(this.$ikey, r.default);
-            processContent(r.default);
-            _.$app.update();
-          });
-        } else {
-          _.$firstDOMNode = context.$firstDOMNode;
-          _.$firstHTMLELement = context.$firstHTMLELement;
-        }
-      } else {
-        _.t(String(contentValue));
-      }
+      _.t(String(contentValue));
     }
   }
 }
