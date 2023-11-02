0c4c880be294bc28f5e837fc44b1b479f101d003
_Kerman <KermanX@qq.com>
2023-11-02 23:06:20 +0800

    only call mdui mutation when updating

--- packages/mdui/src/plugin.ts
+++ packages/mdui/src/plugin.ts
@@ -1,23 +1,26 @@
 import mdui from "mdui";
-import { Plugin } from "refina";
+import { AppState, Plugin } from "refina";
 import { UpdateDialogSize, UpdateMDUI, UpdateSelectItems } from "./symbol";
 import { Dialog } from "mdui/es/components/dialog/class";
 
 const MdUI = new Plugin("mdui", (app) => {
   app.addPermanentHook("afterModifyDOM", () => {
-    if (app.permanentData[UpdateMDUI] === true) {
-      mdui.mutation();
-      app.permanentData[UpdateMDUI] = false;
-    }
+    if (app.state === AppState.update) {
+      if (app.permanentData[UpdateMDUI] === true) {
+        console.warn("Update MDUI");
+        mdui.mutation();
+        app.permanentData[UpdateMDUI] = false;
+      }
 
-    // if (app.permanentData[UpdateSelectItems] === true) {
-    //   mdui.handleUpdate();
-    //   app.permanentData[UpdateSelectItems] = false;
-    // }
+      // if (app.permanentData[UpdateSelectItems] === true) {
+      //   mdui.handleUpdate();
+      //   app.permanentData[UpdateSelectItems] = false;
+      // }
 
-    if (app.permanentData[UpdateDialogSize] !== undefined) {
-      (app.permanentData[UpdateDialogSize] as Dialog).handleUpdate();
-      app.permanentData[UpdateDialogSize] = undefined;
+      if (app.permanentData[UpdateDialogSize] !== undefined) {
+        (app.permanentData[UpdateDialogSize] as Dialog).handleUpdate();
+        app.permanentData[UpdateDialogSize] = undefined;
+      }
     }
   });
 });
