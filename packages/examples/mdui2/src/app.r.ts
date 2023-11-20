bf4b75e9ec164dd817b15351b60b73d88e9f055d
_Kerman <KermanX@qq.com>
2023-11-21 00:05:39 +0800

    update styles of appbar in mdui2 example

--- packages/examples/mdui2/src/app.r.ts
+++ packages/examples/mdui2/src/app.r.ts
@@ -20,141 +20,145 @@ let sliderValue2 = d(60);
 let input = d("Hello");
 
 app.use(MdUI2)(_ => {
-  _._h1({}, "MDUI2 Test");
-
-  _.$css`position:sticky;top:0;left:0;right:0;background-color:#AAA;z-index:10000`;
-  _._div({}, _ => {
-    _._p({}, `Count is ${count}`);
-    _._p({}, `Status is ${status.value}`);
-    _._p({}, `Selected is ${selected.value}`);
-    _._p({}, `Input is ${sliderValue1.value}`);
-  });
-
-  _.embed(componentView, "Avatar", _ => {
-    _.mdAvatar("https://via.placeholder.com/80x80?text=A");
-    _.mdIconAvatar("person");
-  });
-  _.embed(componentView, "Badge", _ => {
-    _.mdBadge();
-    _.t` `;
-    _.mdBadge("99+");
-  });
-  _.embed(componentView, "Button", _ => {
-    _.mdButton("Button") && count++;
-    _.mdButton("Button", true) && count++;
-  });
-  _.embed(componentView, "Button Icon", _ => {
-    _.mdButtonIcon("add") && count++;
-    _.mdButtonIcon("add", true) && count++;
-  });
-  _.embed(componentView, "Checkbox", _ => {
-    _.mdCheckbox(status, "Checkbox");
-    _.mdCheckbox(status, "Checkbox", true);
-  });
-  _.embed(componentView, "Circlular Progress", _ => {
-    _.mdCircularProgress();
-    _.mdCircularProgress(0.8);
-  });
-  _.embed(componentView, "Divider", _ => {
-    _.mdDivider();
-    _.$css`height: 40px; margin-left: 30px`;
-    _._div({}, _ => _.mdVerticalDivider());
-  });
-  _.embed(componentView, "Icon", _ => {
-    _.mdIcon("search");
-    _.mdIcon("delete");
-  });
-  _.embed(componentView, "Linear Progress", _ => {
-    _.mdLinearProgress();
-    _.mdLinearProgress(sliderValue1.value / 100);
-  });
-  _.embed(componentView, "List", _ =>
-    _.mdList(["Item 1", "Item 2", "Item 3"], bySelf, item => {
-      _.t(item);
-    }),
-  );
-  _.embed(componentView, "Radio Group", _ => {
-    _.mdRadioGroup(selected, options);
-    _._br();
-    _.mdRadioGroup(selected, options, [false, true], {
-      "Option 3": _ => _.mdAvatar("https://via.placeholder.com/80x80?text=3"),
+  _.$css`z-index:100000`;
+  _.mdTopAppBar(_ => {
+    _.$css`font-size: 2em; margin-right: 24px`;
+    _._span({}, "MDUI2 Test");
+    _.$cls`margin-top:10px`;
+    _._div({}, _ => {
+      _._span({}, "Status: ");
+      _.$css`font-family:Consolas`;
+      _._div({}, `${count} ${status.value} ${selected.value} ${sliderValue1.value} ${sliderValue2.value}`);
     });
-    _._br();
-    _.mdRadioGroup(selected, options, true);
   });
-  _.embed(componentView, "Range Slider", _ => {
-    _.mdRangeSlider([sliderValue1, sliderValue2]);
-    _.mdRangeSlider([sliderValue1, sliderValue2], false, 10);
-    _.mdRangeSlider([sliderValue1, sliderValue2], true);
-  });
-  _.embed(componentView, "Select", _ => {
-    _.mdSelect(selected, options);
-    _.mdSelect(selected, options, [false, true], {
-      "Option 3": _ => _.mdAvatar("https://via.placeholder.com/80x80?text=3"),
+
+  _.mdLayoutMain(_ => {
+    _.embed(componentView, "Avatar", _ => {
+      _.mdAvatar("https://via.placeholder.com/80x80?text=A");
+      _.mdIconAvatar("person");
     });
-    _.mdSelect(selected, options, true);
-  });
-  _.embed(componentView, "Slider", _ => {
-    _.mdSlider(sliderValue2);
-    _.mdSlider(sliderValue2, false, 10);
-    _.mdSlider(sliderValue2, true);
-  });
-  _.embed(componentView, "Switch", _ => {
-    _.mdSwitch(status);
-    _.mdSwitch(status, true);
-  });
-  _.embed(componentView, "Tabs", _ => {
-    _.mdTabs(
-      "Tab 1",
-      _ => _._p({}, "Content 1"),
-      "Tab 2",
-      _ => _._p({}, "Content 2"),
-      "Tab 3",
-      _ => _._p({}, "Content 3"),
+    _.embed(componentView, "Badge", _ => {
+      _.mdBadge();
+      _.t` `;
+      _.mdBadge("99+");
+    });
+    _.embed(componentView, "Button", _ => {
+      _.mdButton("Button") && count++;
+      _.mdButton("Button", true) && count++;
+    });
+    _.embed(componentView, "Button Icon", _ => {
+      _.mdButtonIcon("add") && count++;
+      _.mdButtonIcon("add", true) && count++;
+    });
+    _.embed(componentView, "Checkbox", _ => {
+      _.mdCheckbox(status, "Checkbox");
+      _.mdCheckbox(status, "Checkbox", true);
+    });
+    _.embed(componentView, "Circlular Progress", _ => {
+      _.mdCircularProgress();
+      _.mdCircularProgress(0.8);
+    });
+    _.embed(componentView, "Divider", _ => {
+      _.mdDivider();
+      _.$css`height: 40px; margin-left: 30px`;
+      _._div({}, _ => _.mdVerticalDivider());
+    });
+    _.embed(componentView, "Icon", _ => {
+      _.mdIcon("search");
+      _.mdIcon("delete");
+    });
+    _.embed(componentView, "Linear Progress", _ => {
+      _.mdLinearProgress();
+      _.mdLinearProgress(sliderValue1.value / 100);
+    });
+    _.embed(componentView, "List", _ =>
+      _.mdList(["Item 1", "Item 2", "Item 3"], bySelf, item => {
+        _.t(item);
+      }),
     );
-  });
-  _.embed(componentView, "Text Field", _ => {
-    _.mdTextField(input);
-    _.mdTextField(input, "Message");
-    _.mdTextField(input, "Message", true);
-  });
-  _.embed(componentView, "Tooltip", _ => {
-    _.mdTooltip("Message", _ => _.mdButton("Hover me!")) && console.log(_.$ev);
-  });
-  _.embed(componentView, "Layouts", _ => {
-    const exampleWindowStyle = "height:400px;border:1px black solid;margin:20px;padding:10px;border-radius:5px;";
-    _.$css(exampleWindowStyle);
-    _.mdLayout(_ => {
-      _.mdTopAppBar("TOP APP BAR");
-      const currentPage = _.mdNavigationBar([
-        ["Home", "home"],
-        ["About", "info"],
-      ]);
-      _.mdLayoutMain(_ => {
-        _.forRange(50, index => _._p({}, `${currentPage} content ${index}`));
+    _.embed(componentView, "Radio Group", _ => {
+      _.mdRadioGroup(selected, options);
+      _._br();
+      _.mdRadioGroup(selected, options, [false, true], {
+        "Option 3": _ => _.mdAvatar("https://via.placeholder.com/80x80?text=3"),
       });
+      _._br();
+      _.mdRadioGroup(selected, options, true);
     });
-    _.$css(exampleWindowStyle);
-    _.mdLayout(_ => {
-      _.mdBottomAppBar("BOTTOM APP BAR");
-      const currentPage = _.mdNavigationRail([
-        ["Home", "home"],
-        ["About", "info"],
-      ]);
-      _.mdLayoutMain(_ => {
-        _.forRange(50, index => _._p({}, `${currentPage} content ${index}`));
+    _.embed(componentView, "Range Slider", _ => {
+      _.mdRangeSlider([sliderValue1, sliderValue2]);
+      _.mdRangeSlider([sliderValue1, sliderValue2], false, 10);
+      _.mdRangeSlider([sliderValue1, sliderValue2], true);
+    });
+    _.embed(componentView, "Select", _ => {
+      _.mdSelect(selected, options);
+      _.mdSelect(selected, options, [false, true], {
+        "Option 3": _ => _.mdAvatar("https://via.placeholder.com/80x80?text=3"),
       });
+      _.mdSelect(selected, options, true);
     });
-    _.$css(exampleWindowStyle);
-    _.mdLayout(_ => {
-      _.mdLayoutMain(_ => {
-        _._h1({}, "Layout Main");
-        _.mdNavigationDrawer(
-          (_, open) => _.mdButton("open drawer") && open(),
-          (_, close) => _.mdButton("close drawer") && close(),
-          true,
-          true,
-        );
+    _.embed(componentView, "Slider", _ => {
+      _.mdSlider(sliderValue2);
+      _.mdSlider(sliderValue2, false, 10);
+      _.mdSlider(sliderValue2, true);
+    });
+    _.embed(componentView, "Switch", _ => {
+      _.mdSwitch(status);
+      _.mdSwitch(status, true);
+    });
+    _.embed(componentView, "Tabs", _ => {
+      _.mdTabs(
+        "Tab 1",
+        _ => _._p({}, "Content 1"),
+        "Tab 2",
+        _ => _._p({}, "Content 2"),
+        "Tab 3",
+        _ => _._p({}, "Content 3"),
+      );
+    });
+    _.embed(componentView, "Text Field", _ => {
+      _.mdTextField(input);
+      _.mdTextField(input, "Message");
+      _.mdTextField(input, "Message", true);
+    });
+    _.embed(componentView, "Tooltip", _ => {
+      _.mdTooltip("Message", _ => _.mdButton("Hover me!")) && console.log(_.$ev);
+    });
+    _.embed(componentView, "Layouts", _ => {
+      const exampleWindowStyle = "height:400px;border:1px black solid;margin:20px;padding:10px;border-radius:5px;";
+      _.$css(exampleWindowStyle);
+      _.mdLayout(_ => {
+        _.mdTopAppBar("TOP APP BAR");
+        const currentPage = _.mdNavigationBar([
+          ["Home", "home"],
+          ["About", "info"],
+        ]);
+        _.mdLayoutMain(_ => {
+          _.forRange(50, index => _._p({}, `${currentPage} content ${index}`));
+        });
+      });
+      _.$css(exampleWindowStyle);
+      _.mdLayout(_ => {
+        _.mdBottomAppBar("BOTTOM APP BAR");
+        const currentPage = _.mdNavigationRail([
+          ["Home", "home"],
+          ["About", "info"],
+        ]);
+        _.mdLayoutMain(_ => {
+          _.forRange(50, index => _._p({}, `${currentPage} content ${index}`));
+        });
+      });
+      _.$css(exampleWindowStyle);
+      _.mdLayout(_ => {
+        _.mdLayoutMain(_ => {
+          _._h1({}, "Layout Main");
+          _.mdNavigationDrawer(
+            (_, open) => _.mdButton("open drawer") && open(),
+            (_, close) => _.mdButton("close drawer") && close(),
+            true,
+            true,
+          );
+        });
       });
     });
   });
