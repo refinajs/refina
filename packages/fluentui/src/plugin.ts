ed3aa1321934bd2f36a67e82dad7e744b501d3ff
_Kerman <KermanX@qq.com>
2023-11-23 15:23:41 +0800

    update fluentui css variables injection

--- packages/fluentui/src/plugin.ts
+++ packages/fluentui/src/plugin.ts
@@ -2,28 +2,23 @@ import { Theme, webDarkTheme, webLightTheme } from "@fluentui/tokens";
 import FIcons from "@refina/fluentui-icons";
 import { Plugin } from "refina";
 
-const currentFThemeSymbol = Symbol("currentFTheme");
-
 const FluentUI = new Plugin(
   "fluentui",
   (app, dark: Theme = webDarkTheme, light: Theme = webLightTheme) => {
     FIcons.install(app);
 
-    app.addPermanentHook("beforeModifyDOM", () => {
-      const theme = light;
-      const currentTheme = app.permanentData[currentFThemeSymbol];
+    const theme = light;
 
-      if (currentTheme === theme) return;
+    let css = `#root{\n`;
+    for (const key in theme) {
+      css += `--${key}:${theme[key as keyof Theme]};\n`;
+    }
+    css += `}`;
 
-      for (const key in theme) {
-        app.root.node.style.setProperty(
-          `--${String(key)}`,
-          String(theme[key as keyof Theme]),
-        );
-      }
-
-      app.permanentData[currentFThemeSymbol] = theme;
-    });
+    const styleElement = document.createElement("style");
+    styleElement.id = "fui-css-variables";
+    styleElement.innerHTML = css;
+    document.head.appendChild(styleElement);
   },
 );
 export default FluentUI;
