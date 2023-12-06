1aa2d79b86827441a23ff6574771c80e1635d095
_Kerman <KermanX@qq.com>
2023-12-06 14:09:48 +0800

    use logger to output ID map

--- packages/vite-plugin/plugin.ts
+++ packages/vite-plugin/plugin.ts
@@ -1,19 +1,25 @@
 import { RefinaTransformer } from "@refina/transformer";
-import type { Plugin } from "vite";
+import type { Plugin, ResolvedConfig } from "vite";
 
 export default function refina(
   transformer: RefinaTransformer = new RefinaTransformer(),
 ) {
+  let config: ResolvedConfig;
   return {
     name: "refina-plugin",
     enforce: "pre",
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
     transform(raw, id) {
       if (!transformer.shouldTransform(id)) {
         return null;
       }
       const { code, map, fileKey } = transformer.transformFile(id, raw);
 
-      console.log(`"${fileKey}"${" ".repeat(3 - fileKey.length)}--> "${id}"`);
+      config.logger.info(
+        `"${fileKey}"${" ".repeat(3 - fileKey.length)}--> "${id}"`,
+      );
 
       return {
         code,
