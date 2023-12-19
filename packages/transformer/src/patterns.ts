ad55988f518238df9e221b7526e896de834cc502
_Kerman <KermanX@qq.com>
2023-12-19 12:49:16 +0800

    fix transformer regexes

--- packages/transformer/src/patterns.ts
+++ packages/transformer/src/patterns.ts
@@ -1,4 +1,4 @@
-export const TEXT_NODE_TAGFUNC = /_\s*\.\s*t\s*`(.*?)`/g;
-export const COMPONENT_FUNC = /_\s*\.\s*([a-zA-Z0-9_]+)\s*\(/g;
+export const TEXT_NODE_TAGFUNC = /\b_\s*\.\s*t\s*`(.*?)`/g;
+export const COMPONENT_FUNC = /\b_\s*\.\s*([a-zA-Z0-9_]+)\s*\(/g;
 export const COMPONENT_FUNC_WITH_TYPE_PARAMS =
-  /_\s*\.\s*([a-zA-Z0-9_]+)\s*\<([\s\S]+?)\>\s*\(/g;
+  /\b_\s*\.\s*([a-zA-Z0-9_]+)\s*\<([\s\S]+?)\>\s*\(/g;
