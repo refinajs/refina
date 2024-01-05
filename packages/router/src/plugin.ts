99fbce24d6cf68095500fc6ee1fb0405af1f2129
_Kerman <KermanX@qq.com>
2024-01-05 16:40:50 +0800

    register `$router` to context

--- packages/router/src/plugin.ts
+++ packages/router/src/plugin.ts
@@ -4,6 +4,7 @@ import { Router } from "./router";
 const RouterPlugin = new Plugin("router", app => {
   const router = new Router(app);
   app.router = router;
+  app.context.$router = router;
   app.pushPermanentHook("afterMain", () => {
     router.updateCurrentPath();
   });
@@ -13,5 +14,8 @@ export default RouterPlugin;
 declare module "refina" {
   interface App {
     router: Router;
+  }
+  interface IntrinsicBaseContext<CS> {
+    $router: Router;
   }
 }
