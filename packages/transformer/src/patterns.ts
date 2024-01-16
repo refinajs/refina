ea9f5312215148c7cf1da642442a824ddde94d43
_Kerman <KermanX@qq.com>
2024-01-16 10:23:17 +0800

    update transformer

--- packages/transformer/src/patterns.ts
+++ packages/transformer/src/patterns.ts
@@ -1,5 +1,5 @@
 export default {
   TEXT_NODE_TAGFUNC: /\b_\s*\.\s*t\s*`(.*?)`/g,
   COMPONENT_FUNC: /\b_\s*\.\s*([a-zA-Z0-9_]+)\s*(?:\<([\s\S]+?)\>)?\s*\(/g,
-  DIRECT_CALL: /(?<![.$_#a-zA-Z0-9])_\s*\(\s*([^{]+?)\s*\)(?![{])/g,
+  DIRECT_CALL: /\b_\s*\(\s*([^{)]+?)\s*\)(?!\s*[{])/g,
 };
