35b2c32aab744959fff4733086b260c943a5576c
_Kerman <kermanx@qq.com>
2024-01-17 20:33:24 +0800

    New translations app.ts (Chinese Simplified)

--- docs/zh/tutorial/src/step-9/_hint/App/App.ts
+++ docs/zh/tutorial/src/step-9/_hint/App/App.ts
@@ -11,24 +11,21 @@ Rewrite the following HTML using low-level rendering functions.
 
 $app(_ => {
   _.$css`position:fixed;`;
-  _._svgSvg(
-    {
-      id: "svg1",
-      width: 100,
-      height: 100,
-    },
-    _ => {
-      _._svgPath({
-        d: "M 10 10 H 90 V 90 H 10 Z",
-        fill: "red",
-      });
-      _._svgCircle({
-        cx: 50,
-        cy: 50,
-        r: 40,
-        stroke: "blue",
-        fill: "none",
-      });
-    },
-  );
+  _._svgSvg({
+    id: "svg1",
+    width: 100,
+    height: 100
+  }, _ => {
+    _._svgPath({
+      d: "M 10 10 H 90 V 90 H 10 Z",
+      fill: "red"
+    });
+    _._svgCircle({
+      cx: 50,
+      cy: 50,
+      r: 40,
+      stroke: "blue",
+      fill: "none"
+    });
+  });
 });
