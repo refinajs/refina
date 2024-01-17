2673b898c9c5c6e0cd27b8272150dc7865f4c1d6
_Kerman <kermanx@qq.com>
2024-01-17 20:33:16 +0800

    New translations app.ts (Chinese Simplified)

--- docs/zh/tutorial/src/step-5/_hint/App/App.ts
+++ docs/zh/tutorial/src/step-5/_hint/App/App.ts
@@ -1,8 +1,6 @@
 import { $app } from "refina";
 import Basics from "@refina/basic-components";
-
 let count = 0;
-
 $app.use(Basics)(_ => {
   _.h1(`You have clicked the button ${count} times.`);
   if (_.button("Click me!")) {
