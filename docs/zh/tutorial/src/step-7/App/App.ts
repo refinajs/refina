730121a84acf51785cc6bf8a8290bd0ff4b86c55
_Kerman <kermanx@qq.com>
2024-01-17 20:33:19 +0800

    New translations app.ts (Chinese Simplified)

--- docs/zh/tutorial/src/step-7/App/App.ts
+++ docs/zh/tutorial/src/step-7/App/App.ts
@@ -1,35 +1,27 @@
 import { $app, model } from "refina";
 import Basics from "@refina/basic-components";
-
 let id = 0;
 let newTodo = model("");
-let todos = [
-  {
-    id: id++,
-    text: "Learn HTML",
-  },
-  {
-    id: id++,
-    text: "Learn JavaScript",
-  },
-  {
-    id: id++,
-    text: "Learn Refina",
-  },
-];
-
+let todos = [{
+  id: id++,
+  text: "Learn HTML"
+}, {
+  id: id++,
+  text: "Learn JavaScript"
+}, {
+  id: id++,
+  text: "Learn Refina"
+}];
 function remove(id: number) {
   todos = todos.filter(todo => todo.id !== id);
 }
-
 $app.use(Basics)(_ => {
   _.textInput(newTodo);
-  _.button("Add Todo") &&
-    todos.push({
-      id: id++,
-      text: newTodo.value,
-    });
+  _.button("Add Todo") && todos.push({
+    id: id++,
+    text: newTodo.value
+  });
 
-  // 完成以下渲染 todo 列表的代码
+  // Finish the following code to render the list of todos.
   // _.ul(todos, ...);
 });
