ca731d2fa26560ef76204cc6f6617999f6b021cc
_Kerman <kermanx@qq.com>
2024-01-17 15:12:38 +0800

    New translations app.ts (Chinese Simplified)

---
+++ docs/zh/tutorial/src/step-8/_hint/App/App.ts
@@ -0,0 +1,10 @@
+import { $app } from "refina";
+import Basics from "@refina/basic-components";
+import PartView from "./PartView";
+$app.use(Basics)(_ => {
+  _.h1("Title");
+  _(PartView)("Part 1", "Content 1");
+  _(PartView)("Part 2", "Content 2");
+  _(PartView)("Part 3", "Content 3");
+  _(PartView)("Part 4", "Content 4");
+});
