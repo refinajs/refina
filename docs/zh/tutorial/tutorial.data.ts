627f723a8fbecf0d929ac720bb4576c431d185da
_Kerman <kermanx@qq.com>
2024-01-18 09:44:43 +0800

    New translations tutorial.data.ts (Chinese Simplified)

--- docs/zh/tutorial/tutorial.data.ts
+++ docs/zh/tutorial/tutorial.data.ts
@@ -1,10 +1,8 @@
 import * as path from "path";
 import { createMarkdownRenderer } from "vitepress";
-import { readExamples } from "../../helpers/loader";
-import type { ExampleData } from "../../helpers/utils";
-
+import { readExamples } from "../helpers/loader";
+import type { ExampleData } from "../helpers/utils";
 export declare const data: Record<string, ExampleData>;
-
 export default {
   watch: "./src/**",
   async load() {
@@ -12,11 +10,11 @@ export default {
     const files = readExamples(path.resolve(__dirname, "./src"));
     for (const step in files) {
       const stepFiles = files[step];
-      const desc = stepFiles["description.md"] as string;
+      const desc = (stepFiles["description.md"] as string);
       if (desc) {
         stepFiles["description.md"] = md.render(desc);
       }
     }
     return files;
-  },
+  }
 };
