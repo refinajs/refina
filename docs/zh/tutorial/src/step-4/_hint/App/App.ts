a2ec1412be426687a7aa0bc6052e109ff515bfb0
_Kerman <kermanx@qq.com>
2024-01-24 18:06:43 +0800

    New translations app.ts (Chinese Simplified)

--- docs/zh/tutorial/src/step-4/_hint/App/App.ts
+++ docs/zh/tutorial/src/step-4/_hint/App/App.ts
@@ -1,8 +1,13 @@
 import { $app } from "refina";
 import Basics from "@refina/basic-components";
 let count = 0;
-$app.use(Basics)(_ => {
+$app([Basics], _ => {
   // Not here!
 
   _.p(`Count is ${count}!`);
 });
+declare module "refina" {
+  interface Plugins {
+    Basics: typeof Basics;
+  }
+}
