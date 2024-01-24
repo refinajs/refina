db8604c972da68530a6a25ea83a40eee73fd3572
_Kerman <kermanx@qq.com>
2024-01-24 18:06:44 +0800

    New translations app.ts (Chinese Simplified)

--- docs/zh/tutorial/src/step-5/App/App.ts
+++ docs/zh/tutorial/src/step-5/App/App.ts
@@ -1,6 +1,11 @@
 import { $app } from "refina";
 import Basics from "@refina/basic-components";
-$app.use(Basics)(_ => {
+$app([Basics], _ => {
   _.h1(`You have clicked the button 0 times.`);
   _.button("Click me!");
 });
+declare module "refina" {
+  interface Plugins {
+    Basics: typeof Basics;
+  }
+}
