d4d04446d0d0c4dbd3b1282f7111882e9866e3e1
_Kerman <KermanX@qq.com>
2024-01-26 16:28:16 +0800

    add `target` param to `_.a`

--- packages/basic-components/src/components/a.ts
+++ packages/basic-components/src/components/a.ts
@@ -1,7 +1,7 @@
 import { Component, Content, _ } from "refina";
 
 export class BasicA extends Component {
-  $main(inner: Content, href: string): void {
-    _._a({ href }, inner);
+  $main(inner: Content, href: string, target?: string): void {
+    _._a({ href, target }, inner);
   }
 }
